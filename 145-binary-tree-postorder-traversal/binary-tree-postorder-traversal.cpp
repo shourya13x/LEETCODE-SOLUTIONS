b5ad33cb336e3d90b4222f1f3aefd4f19ae6ef0d
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-02-17 19:46:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (92.04%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -6,28 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st1,st2;
         vector<int> postorder;
-        if(root==NULL)return postorder;
-        st1.push(root);
-        while(!st1.empty())
-        {
-            root=st1.top();
-            st1.pop();
-            st2.push(root);
-            if(root->left!=NULL) st1.push(root->left);
-             if(root->right!=NULL) st1.push(root->right);
-        }
-        while(!st2.empty())
-        {
-             postorder.push_back(st2.top()->val);
-             st2.pop();
+        TreeNode *curr=root;
+          TreeNode *  temp;
+        stack<TreeNode*> st;
+          if(root==NULL)  return postorder;
+        while (curr != NULL || !st.empty()) {
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                temp = st.top()->right;
+                if (temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    postorder.push_back(temp->val);
+
+                    while (!st.empty()&& temp ==st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        postorder.push_back(temp->val);
+                    }
+                } else curr = temp;
+                    
+            }
         }
         return postorder;
     }
