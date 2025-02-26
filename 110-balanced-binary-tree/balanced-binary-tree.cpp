60dd6a2c72ce96b8831441c5c07f1e0f8f142538
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-02-27 01:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (24.03%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,36 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int findDepth(TreeNode* root) {
-        if (root == NULL) {
-            return 0;
-        }
-        int Lh=findDepth(root->left);
-        int Rh=findDepth(root->right);
-
-        return 1+max(Lh,Rh);
+    bool isBalanced(TreeNode* root) {
+        return dfsheight(root)!=-1;
+        
     }
-    bool isBalanced(TreeNode* root) 
+    int dfsheight(TreeNode* root)
     {
-        if(root==NULL) return true;
-
-     int Lh = findDepth(root->left);
-      int Rh = findDepth(root->right);
-      if(abs(Rh-Lh)>1) return false;
-        bool left= isBalanced(root->left);
-        bool right= isBalanced(root->right);
-
-        if(!left || !right) return false;
-
-
-        return true;
-
-
+        if(root==NULL) return 0;
+        int leftheight = dfsheight(root->left);
+            if(leftheight==-1) return -1;
+        int rightheight = dfsheight(root->right);
+            if(rightheight==-1) return -1;
+        if(abs(leftheight-rightheight)>1) return -1;
+        return 1+max(leftheight,rightheight);
     }
 };
