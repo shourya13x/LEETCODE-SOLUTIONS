52a4656f0372fe4fbe90d6a94dea188415b6d2d8
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-03-04 12:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (99.88%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -11,23 +11,25 @@
  */
 class Solution {
 public:
-    void tt(TreeNode* root, int level, vector<vector<int>>&nums){
-        if(root==NULL){
-            return;
-        }
-        if(nums.size()<=level){
-            nums.push_back({});
-        }
 
-        nums[level].push_back(root->val);
+        int maxdepth=-1;
+        int bottomleft;
+    void helper(TreeNode* root,int currdepth)
+    {
+          if(!root) return;
+          if(currdepth>maxdepth)
+          {
+            maxdepth=currdepth;
+            bottomleft=root->val;
+          }
+          helper(root->left,currdepth+1);
+          helper(root->right,currdepth+1);
 
-        tt(root->right,level+1,nums);
-        tt(root->left,level+1,nums);
 
     }
     int findBottomLeftValue(TreeNode* root) {
-       vector<vector<int>>nums;
-        tt(root,0,nums);
-        return nums.back().back();
+       helper(root,0);
+       return bottomleft;
+
     }
 };
