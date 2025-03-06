66ee8ac2d1bc3aac91f0e134a5d1fe0b83b1dc7c
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-03-06 18:56:42 +0530

    Time: 2 ms (62.50%) | Memory: 11.3 MB (38.40%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {
-            return a.second < b.second;
-        };
-
-        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)>
-            pq(cmp);
-
-        unordered_map<char, int> hm;
-
-        for (char c : s) {
-            hm[c]++;
-        }
-
-        for (const auto& entry : hm) {
-            pq.push(make_pair(entry.first, entry.second));
-        }
-
+        int n=s.size();
         string result = "";
-        while (!pq.empty()) {
-            pair<char, int> p = pq.top();
-            pq.pop();
-            result.append(p.second, p.first);
+      unordered_map<char,int>freq;
+      for(auto it:s)
+      {
+        freq[it]++;
+      }  
+      while (!freq.empty()) {
+            char maxChar = '\0';  // Character with the highest frequency
+            int maxFreq = 0;
+
+            // Find the character with the highest frequency
+            for (auto& pair : freq) {
+                if (pair.second > maxFreq) {
+                    maxFreq = pair.second;
+                    maxChar = pair.first;
+                }
+            }
+
+            // Append maxFreq occurrences of maxChar to result
+            result.append(maxFreq, maxChar);
+
+            // Remove the character from the map
+            freq.erase(maxChar);
         }
 
-        return result;
+        return result;   
+      
     }
 };
