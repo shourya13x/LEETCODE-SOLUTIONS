f7a76e2d1e1af04bf1796766afe171b355d70523
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-03-21 10:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (78.10%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -6,26 +6,16 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void helper(TreeNode* node, vector<int>& pre) {
-        if (node == NULL)
-        {
-            pre.push_back(INT_MIN);
-             return;
-        }
-        pre.push_back(node->val);
-        helper(node->left, pre);
-        helper(node->right, pre);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> pre1,pre2;
-        helper(p, pre1);
-        helper(q, pre2);
-        return pre1==pre2;
+        if(p==NULL || q==NULL)
+        {
+              return (p==q);
+        }
+        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
