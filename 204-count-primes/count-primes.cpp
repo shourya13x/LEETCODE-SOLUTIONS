c49afc18071be233160ae49d8a560ba43a1bf634
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-04-04 09:50:55 +0530

    Time: 160 ms (84.99%) | Memory: 132.6 MB (8.71%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+     vector<int>p(n,1);
+     for(int i=2;i<=sqrt(n);i++)
+     {
+        if(p[i]==1)
+        {
+            for(int j=i*i;j<n;j+=i)
+            {
+                p[j]=0;
+            }
+        }
+     }
+     int count=0;
+     for(int i=2;i<n;i++)
+     {
+        if(p[i]==1)
+        {
+            count++;
+        }
+
+     }
+
+return count;
+
+    }
+};
