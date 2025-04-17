ef3d9aad80a8328e1ce48e5c4b45a1fa0d8652c9
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-04-17 23:40:27 +0530

    Time: 1 ms (59.42%) | Memory: 16.6 MB (18.09%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,39 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>> ans; 
-
-        queue<pair<TreeNode*,int>> Q; // node and col
-        Q.push({root,0});
-        int depth=0;
-        while(!Q.empty()){
-            int s=Q.size();
-            while(s--){
-                auto [node,col]=Q.front();
-                Q.pop();
-                ans.push_back({col,depth,node->val});
-                if(node->left!=nullptr) Q.push({node->left,col-1});
-                if(node->right!=nullptr) Q.push({node->right,col+1});
+        map<int,map<int,multiset<int>>> nodes;
+        queue<pair<TreeNode*,pair<int,int>>>todo;
+        todo.push({root, {0, 0}});
+        while(!todo.empty())
+        {
+            auto p=todo.front();
+            todo.pop();
+            TreeNode* node=p.first;
+            int x=p.second.first,y=p.second.second;
+            nodes[x][y].insert(node->val);
+            if(node->left)
+            {
+                todo.push({node->left,{x-1,y+1}});
             }
-            depth++;
-        }
-
-        sort(ans.begin(),ans.end());
-
-        vector<vector<int>> final;
-        vector<int> temp;
-        int curr=ans[0][0];
-        for(int i=0;i<ans.size();i++){
-            if(ans[i][0]==curr) temp.push_back(ans[i][2]);
-            else{
-                final.push_back(temp);
-                temp.clear();
-                curr=ans[i][0];
-                temp.push_back(ans[i][2]);
+            if(node->right)
+            {
+                todo.push({node->right,{x+1,y+1}});
             }
         }
-        if(!temp.empty()) final.push_back(temp);
-        return final;
-
+        vector<vector<int>>ans;
+        for(auto p:nodes){
+            vector<int>col;
+            for(auto q:p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
+            }
+            ans.push_back(col);
+        }
+        return ans;
     }
 };
