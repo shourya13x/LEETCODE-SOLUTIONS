24c8095e9e04eb5e8c4210da7c77928a07ae8471
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-04-27 16:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (84.36%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,26 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void right(TreeNode* root, int level, vector<int> &ans) {
-        if (root == NULL)
-            return;
-        if (level == ans.size()){
-            ans.push_back(root->val);
-        }
-        right(root->right,level+1,ans);
-        right(root->left,level+1,ans);
-
-    }
     vector<int> rightSideView(TreeNode* root) {
-        
-        vector<int>ans;
-        right(root,0,ans);
+        vector<int> ans;
+        queue<TreeNode*> q;
+        if(root==NULL) return ans;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n=q.size();
+            TreeNode * node=NULL;
+            for(int i=0;i<n;i++)
+            {
+                node=q.front();
+                q.pop();
+                if(node->left!=NULL)
+                {
+                    q.push(node->left);
+                }
+                if(node->right!=NULL)
+                {
+                    q.push(node->right);
+                }
+            
+            }
+            ans.push_back(node->val);
+            
+        }
         return ans;
     }
 };
