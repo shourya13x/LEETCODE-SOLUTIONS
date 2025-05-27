729d6bcaeff240ed8d7dd65a87a9fbf583d69391
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-05-27 18:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (18.36%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -6,29 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
- public:
-  TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-    unordered_map<int, int> inToIndex;
-    for (int i = 0; i < inorder.size(); ++i)
-      inToIndex[inorder[i]] = i;
-    return build(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1, inToIndex);
-  }
+public:
+    TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int posstart,
+                     int posend, int instart, int inend, unordered_map<int, int> &mpp) {
+        if (posstart > posend || instart > inend) {
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(postorder[posend]);
+        int inroot=mpp[root->val];
+        int numsleft=inroot-instart;
+      root->left = helper(inorder, postorder,
+                    posstart, posstart + numsleft - 1,
+                    instart, inroot - 1, mpp);
 
- private:
-  TreeNode* build(const vector<int>& inorder, int inStart, int inEnd, const vector<int>& postorder, int postStart, int postEnd, const unordered_map<int, int>& inToIndex) {
-    if (inStart > inEnd)
-      return nullptr;
+root->right = helper(inorder, postorder,
+                     posstart + numsleft, posend - 1,
+                     inroot + 1, inend, mpp);
 
-    const int rootVal = postorder[postEnd];
-    const int rootInIndex = inToIndex.at(rootVal);
-    const int leftSize = rootInIndex - inStart;
-    TreeNode* root = new TreeNode(rootVal);
-    root->left = build(inorder, inStart, rootInIndex - 1, postorder, postStart, postStart + leftSize - 1, inToIndex);
-    root->right = build(inorder, rootInIndex + 1, inEnd, postorder, postStart + leftSize, postEnd - 1, inToIndex);
-    return root;
-  }
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int, int> mpp;
+        int n=inorder.size();
+        for (int i = 0; i< n; i++) {
+            mpp[inorder[i]] = i;
+        }
+        return helper(inorder,postorder,0,postorder.size()-1,0,inorder.size()-1,mpp);
+    }
 };
