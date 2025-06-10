aa8ab0f2d4f5684b11b5f96b85b5b36c315bdec5
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-06-10 12:23:45 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (47.07%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,22 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-
-bool isPossible(TreeNode* root, long long l, long long r){
-    if(root == nullptr)  return true;
-    if(root->val < r and root->val > l)
-        return isPossible(root->left, l, root->val) and 
-                                isPossible(root->right, root->val, r);
-    else return false;
-}
-
 public:
+    bool validtreechecker(TreeNode* root, long long INTMIN, long long INTMAX) {
+        if (root == NULL)
+            return true;
+        if (root->val >= INTMAX || root->val <= INTMIN)
+            return false;
+
+        return validtreechecker(root->left, INTMIN, root->val)
+        && validtreechecker(root->right, root->val, INTMAX);
+    }
     bool isValidBST(TreeNode* root) {
-        long long int min = -1000000000000, max = 1000000000000;
-        return isPossible(root, min, max);
+        return validtreechecker(root, LLONG_MIN, LLONG_MAX);
     }
 };
