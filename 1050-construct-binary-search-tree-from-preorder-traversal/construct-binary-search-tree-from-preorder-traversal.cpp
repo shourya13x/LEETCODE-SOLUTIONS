702ce53fc413d155f45eda2f7239a418c53ed668
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-06-17 00:02:06 +0530

    Time: 55 ms (4.19%) | Memory: 36.1 MB (5.02%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,23 +12,32 @@
  */
 class Solution {
 public:
-    TreeNode* insert(TreeNode* root,int val)
-    {
-        if(root==NULL) return new TreeNode(val);
-        if(val<root->val)
-        {
-            root->left= insert(root->left,val);
-        }
-        else{
-            root->right=insert(root->right,val);
-        }
-        return root;
+    TreeNode* buildtree(vector<int>& preorder, vector<int> inorder,
+                        int prestart, int preend, int instart, int inend,
+                        unordered_map<int, int> mpp) {
+        if (prestart > preend || instart > inend)
+            return nullptr;
+
+        TreeNode* currroot = new TreeNode(preorder[prestart]);
+        int inroot = mpp[currroot->val];
+        int numsleft = inroot - instart;
+
+        currroot->left = buildtree(preorder, inorder, prestart + 1,
+                               prestart + numsleft, instart, inroot - 1, mpp);
+        currroot->right = buildtree(preorder, inorder, prestart + numsleft + 1,
+                                preend, inroot + 1, inend, mpp);
+
+        return currroot;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode* root = nullptr;
-        for (int val : preorder) {
-            root = insert(root, val);
+        vector<int> inorder = preorder;
+        sort(inorder.begin(), inorder.end());
+        unordered_map<int, int> mpp;
+        int n = inorder.size();
+        for (int i = 0; i < n; i++) {
+            mpp[inorder[i]] = i;
         }
-        return root;
+        return buildtree(preorder, inorder, 0, preorder.size() - 1, 0,
+                         inorder.size() - 1, mpp);
     }
 };
