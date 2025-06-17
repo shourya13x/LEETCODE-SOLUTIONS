a34783f44c07772d59f6a8b8914d0d77b9455ba7
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-06-17 10:54:02 +0530

    Time: 3 ms (19.60%) | Memory: 16.3 MB (69.20%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -6,38 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* buildtree(vector<int>& preorder, vector<int> inorder,
-                        int prestart, int preend, int instart, int inend,
-                        unordered_map<int, int> mpp) {
-        if (prestart > preend || instart > inend)
-            return nullptr;
-
-        TreeNode* currroot = new TreeNode(preorder[prestart]);
-        int inroot = mpp[currroot->val];
-        int numsleft = inroot - instart;
-
-        currroot->left = buildtree(preorder, inorder, prestart + 1,
-                               prestart + numsleft, instart, inroot - 1, mpp);
-        currroot->right = buildtree(preorder, inorder, prestart + numsleft + 1,
-                                preend, inroot + 1, inend, mpp);
-
-        return currroot;
+    TreeNode* build(vector<int>& A,int& i,int bound)
+    {
+        if(i==A.size()|| A[i] > bound) return NULL;
+        TreeNode* node= new TreeNode(A[i++]);
+        node->left=build(A,i,node->val);
+        node->right=build(A,i,bound);
+        return node;
     }
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector<int> inorder = preorder;
-        sort(inorder.begin(), inorder.end());
-        unordered_map<int, int> mpp;
-        int n = inorder.size();
-        for (int i = 0; i < n; i++) {
-            mpp[inorder[i]] = i;
-        }
-        return buildtree(preorder, inorder, 0, preorder.size() - 1, 0,
-                         inorder.size() - 1, mpp);
+    TreeNode* bstFromPreorder(vector<int>& A) {
+        int i=0;
+        return build(A,i,INT_MAX);
     }
 };
