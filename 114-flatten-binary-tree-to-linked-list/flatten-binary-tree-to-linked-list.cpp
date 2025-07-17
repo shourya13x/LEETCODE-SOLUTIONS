b1766258cb733f7d5c1f47e9c4b0edb8f16ded4e
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2025-07-18 00:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (77.07%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -6,25 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     void flatten(TreeNode* root) {
+         if (!root) return;
+
+        flatten(root->left);
+        flatten(root->right);
+
+        TreeNode* temp = root->right;
+        root->right = root->left;
+        root->left = nullptr;
+
         TreeNode* curr = root;
-        while (curr != NULL) {
-            if (curr->left != NULL) {
-                TreeNode* prev = curr->left;
-                while (prev->right) {
-                    prev = prev->right;
-                }
-                prev->right = curr->right;
-                curr->right = curr->left;
-                curr->left=NULL;
-            }
-            curr=curr->right;
+        while (curr->right) {
+            curr = curr->right;
         }
+        curr->right = temp;
     }
 };
