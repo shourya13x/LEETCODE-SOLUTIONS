48f60d2d112e4c3871ca80dc9b301aaef278a273
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2026-05-23 04:58:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int low=0;
-        int high=n-1;
-        while(low<=high)
-        {
-            int mid =(low+high)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[low]<=nums[mid])
-            {
-                if(target>=nums[low] && target <=nums[mid])
-                {
-                    high=mid-1;
-                }
-                else
-                {
-                    low=mid+1;
-                }
-                
-            }
-            else
-            {
-                if(target>=nums[mid] && target<=nums[high])
-                {
-                    low=mid+1;
-                }
-                else
-                {
-                    high=mid-1;
-                }
-            }
+        int n = nums.size();
+        int lo = 0, hi = n - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums.back()) lo = mid + 1;
+            else hi = mid;
         }
+
+        int rot = lo;
+        lo = 0, hi = n - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int real = (mid + rot) % n;
+
+            if (nums[real] == target)
+                return real;
+
+            if (nums[real] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+
         return -1;
     }
 };
